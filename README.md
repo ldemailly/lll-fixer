3c74338766dc6d9003d8bf3a42e8215decce05d1
Laurent Demailly <ldemailly@gmail.com>
2024-03-20 21:05:06 -0700

    Initial / start of implementation, already showing an odd bug

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lll-fixer
 Fix lll (line length limit) lines too long linter errors in go files
+
+Currently weird bug with `-len 79`: (2 new lines somehow)
+
+```diff
+21:02:56 main lll-fixer/$ colordiff -u lll_fixer.go lll_fixer.go.lll
+--- lll_fixer.go	2024-03-20 21:02:46.473102471 -0700
++++ lll_fixer.go.lll	2024-03-20 21:02:56.382460319 -0700
+@@ -24,7 +24,9 @@
+ 	}
+ }
+
+-// process modifies the file filename to split long comments at maxlen. making this line longer than 80 characters to test the lll fixer itself. fun no?
++// process modifies the file filename to split long comments at maxlen. making
++//
++//	this line longer than 80 characters to test the lll fixer itself. fun no?
+ func process(filename string, maxlen int) {
+ 	// Parse the Go file
+ 	fset := token.NewFileSet()
+```
---
+++ lll_fixer.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"os"
+
+	"fortio.org/cli"
+	"fortio.org/log"
+)
+
+func main() {
+	maxlen := flag.Int("len", 79, "max line length") // 78 or 80 or 132 works but... somehow 79 is creating 2 new lines(!)
+	cli.MinArgs = 1
+	cli.MaxArgs = -1
+	cli.ArgsHelp = "filenames..."
+	cli.Main()
+	for _, filename := range flag.Args() {
+		process(filename, *maxlen)
+		// Run gofumpt on the modified file
+	}
+}
+
+// process modifies the file filename to split long comments at maxlen. making this line longer than 80 characters to test the lll fixer itself. fun no?
+func process(filename string, maxlen int) {
+	// Parse the Go file
+	fset := token.NewFileSet()
+	log.Infof("Processing file %q", filename)
+	node, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	if err != nil {
+		log.Errf("Error parsing %q: %v", filename, err)
+		return
+	}
+
+	// Traverse and modify the AST
+	ast.Inspect(node, func(n ast.Node) bool {
+		// Split long comments
+		if c, ok := n.(*ast.Comment); ok {
+			if len(c.Text) > maxlen {
+				c.Text = c.Text[:maxlen-1] + "\n// " + c.Text[maxlen-1:]
+			}
+		}
+		return true
+	})
+
+	// Generate the modified code
+	newname := filename + ".lll"
+	f, err := os.Create(newname)
+	if err != nil {
+		log.Errf("Error creating modified file %q: %v", newname, err)
+	}
+	defer f.Close()
+	if err := format.Node(f, fset, node); err != nil {
+		log.Errf("Error outputting modified file: %v", err)
+	}
+	log.Infof("Modified file written to %q", newname)
+}
